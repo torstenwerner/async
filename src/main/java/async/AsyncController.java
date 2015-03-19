62f595cd5f90b621510ffd2fda503dfbc6cf06c3
Torsten Werner <torsten.werner@westernacher.com>
2015-03-19 20:03:58 +0100

    use AsyncRestTemplate

--- src/main/java/async/AsyncController.java
+++ src/main/java/async/AsyncController.java
@@ -1,12 +1,25 @@
 package async;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestOperations;
+import org.springframework.web.client.AsyncRestTemplate;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @RestController
 public class AsyncController {
+    private final AsyncRestOperations restOperations = new AsyncRestTemplate();
+
+    static class Response extends HashMap<String, String> {
+    }
+
     @RequestMapping("/")
-    public String hello() {
-        return "Hello!";
+    public Response github() throws ExecutionException, InterruptedException {
+        final Future<ResponseEntity<Response>> future = restOperations.getForEntity("https://api.github.com", Response.class);
+        return future.get().getBody();
     }
 }
